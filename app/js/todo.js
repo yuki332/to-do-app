4021148da3669e93b7ab584a87087116adbbb32d
Yuki Arai <sk80332@gmail.com>
2016-04-26 12:27:09 -0600

    update js file

--- app/js/todo.js
+++ app/js/todo.js
@@ -1,47 +1,60 @@
-(function(document) {
-  'use strict';
+// (function(document) {
 
-  var app = document.querySelector('#app');
-  app.firebaseURL = 'https://dgm-todo-app.firebaseio.com';
-  app.firebaseProvider = 'google';
+function test(){
+  console.log("todojs");
+};
 
-  app.items = [];
+var app = document.querySelector('#to-do-app');
 
-  app.updateItems = function(snapshot) {
-    this.items = [];
-    snapshot.forEach(function(childSnapshot) {
-      var item = childSnapshot.val();
-      item.uid = childSnapshot.key();
-      this.push('items', item);
-    }.bind(this));
-  };
-
-  app.addItem = function(event) {
-    event.preventDefault(); // Don't send the form!
-    this.ref.push({
-      done: false,
-      text: app.newItemValue
-    });
-    app.newItemValue = '';
-  };
-
-  app.toggleItem = function(event) {
-    this.ref.child(event.model.item.uid).update({done: event.model.item.done});
-  };
-
-  app.deleteItem = function(event) {
-    this.ref.child(event.model.item.uid).remove();
-  };
-
-  app.onFirebaseError = function(e) {
-    this.$.errorToast.text = e.detail.message;
-    this.$.errorToast.show();
-  };
-  app.onFirebaseLogin = function(e) {
-    this.ref = new Firebase(this.firebaseURL + '/user/' + e.detail.user.uid);
-    this.ref.on('value', function(snapshot) {
-      app.updateItems(snapshot);
-    });
-  };
-
-})(document);
+function updateItems (snapshot){
+  console.log("update");
+var foo = snapshot.val();
+var task = foo.task;
+console.log(foo);
+}
+//
+//   // 'use strict';
+//   //
+//   // var app = document.querySelector('#to-do-app');
+//   // app.firebaseURL = 'https://dgm-todo-app.firebaseio.com';
+//   //
+//   // app.items = [];
+//   //
+//   // app.updateItems = function(snapshot) {
+//   //   this.items = [];
+//   //   snapshot.forEach(function(childSnapshot) {
+//   //     var item = childSnapshot.val();
+//   //     item.uid = childSnapshot.key();
+//   //     this.push('items', item);
+//   //   }.bind(this));
+//   // };
+//   //
+//   // app.addItem = function(event) {
+//   //   event.preventDefault(); // Don't send the form!
+//   //   this.ref.push({
+//   //     done: false,
+//   //     text: app.newItemValue
+//   //   });
+//   //   app.newItemValue = '';
+//   // };
+//   //
+//   // app.toggleItem = function(event) {
+//   //   this.ref.child(event.model.item.uid).update({done: event.model.item.done});
+//   // };
+//   //
+//   // app.deleteItem = function(event) {
+//   //   this.ref.child(event.model.item.uid).remove();
+//   // };
+//   //
+//   // app.onFirebaseError = function(e) {
+//   //   this.$.errorToast.text = e.detail.message;
+//   //   this.$.errorToast.show();
+//   // };
+//   // app.onFirebaseLogin = function(e) {
+//   //   this.ref = new Firebase(this.firebaseURL + '/user/' + e.detail.user.uid);
+//   //   this.ref.on('value', function(snapshot) {
+//   //     app.updateItems(snapshot);
+//   //   });
+//   // };
+//   //
+// })(document);
